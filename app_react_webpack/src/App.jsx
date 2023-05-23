e881b806b71768410a1f32af939f40d7f69e73f8
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-05-23 10:55:02 -0400

    Update with bootstrap

--- app_react_webpack/src/App.jsx
+++ app_react_webpack/src/App.jsx
@@ -11,6 +11,17 @@ const App = () => {
                 A
             </div>
         </div>
+        <div className="card w-50">
+            <div className="card-header">
+                <h1 className="card-title">Home</h1>
+            </div>
+            <div className="card-body">
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente maiores consequatur asperiores natus facilis iste atque earum modi adipisci, a ullam pariatur, odio aut obcaecati at nesciunt molestias numquam optio.</p>
+            </div>
+            <div className="card-footer">
+                <button className="btn btn-primary">More Info</button>
+            </div>
+        </div>
         </>
     )
 }
